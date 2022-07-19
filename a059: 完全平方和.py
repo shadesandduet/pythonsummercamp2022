2f5a51db25cdc665bf3f4e9dece14f6411030b3c
shadesandduet <91614232+shadesandduet@users.noreply.github.com>
2022-07-19 15:16:00 +0800

    Update a059: 完全平方和.py

--- a059: 完全平方和.py
+++ a059: 完全平方和.py
@@ -1,1 +1,10 @@
-
+n = int(input())           
+for i in range(n):
+  a = int(input())          
+  b = int(input())          
+  result = []               
+  for j in range(a, b+1):   
+    j2 = j**0.5 * 100      
+    if j2%100==0:           
+      result.append(j)      
+  print(f'Case {i+1}: {sum(result)}')
