ebc94d45edfa2037f33c07800d72f667277bb77a
shadesandduet <91614232+shadesandduet@users.noreply.github.com>
2022-07-19 15:54:35 +0800

    Update a121: 質數又來囉.py

--- a121: 質數又來囉.py
+++ a121: 質數又來囉.py
@@ -1,1 +1,17 @@
+a = int(input())
+b = int(input())
 
+def count_Primes_nums(n):
+    kaishi = 0
+    for num in range(a,b+1):
+        if num <= 1:
+            continue
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            kaishi += 1
+
+    return kaishi
+    
+print(count_Primes_nums(n))
